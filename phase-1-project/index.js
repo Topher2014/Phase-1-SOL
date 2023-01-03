de66a3e50d2a092d19767b09f26a4159e31740da
samchappel <118487193+samchappel@users.noreply.github.com>
2023-01-03 09:48:17 -0800

    Add files via upload

---
+++ phase-1-project/index.js
@@ -0,0 +1,18 @@
+// GLOBAL
+const sunriseList = document.querySelector('#sunrise-list')
+// Defining text characters for the empty and full hearts for you to use later.
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
+
+// FETCH FUNCTIONS
+function getAllSunrises() {
+    fetch('https://sunrisesunset.io/api/')
+    .then(response => response.json())
+    .then(console.log)
+}
+
+// RENDER FUNCTIONS
+function addLikeBtn() {
+    const btn = sunriseList.createElement('btn')
+    btn.textContent = 'Like'
+}
