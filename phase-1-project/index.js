c9793eb352fe1a4c127d621cd3463b812bf4a536
Sam Chappel <m.samchappel@gmail.com>
2023-01-03 19:07:08 -0800

    hardcode HTML removed

--- phase-1-project/index.js
+++ phase-1-project/index.js
@@ -2,19 +2,51 @@
 const baseUrl = "https://api.sunrisesunset.io/json?"
 const endUrl = "&date=today"
 const sunriseList = document.querySelector('#sunrise-list')
+const URL = 'http://localhost:3000/parks'
 
 
 // FETCH FUNCTIONS
 function getAllSunrises(lat, lng, tmz) {
     return fetch(baseUrl + `lat=${lat}&lng=${lng}&timezone=${tmz}` + endUrl)
         .then(response => response.json())
-        .then(console.log)
+        // .then(renderNationalParks)
 }
 
-https://api.sunrisesunset.io/json?lat=38.907192&lng=-77.036873&timezone=UTC&date=today
+function getNationalParks(url) {
+    return fetch(url)
+        .then(response => response.json())
+}
+
 
 // RENDER FUNCTIONS
-function addLikeBtn() {
-    const btn = sunriseList.createElement('btn')
-    btn.textContent = 'Like'
+function renderNationalParks(parksObj) {
+    const li = document.createElement('li')
+    li.className = "list-li"
+    const image = document.createElement('img')
+    image.src = parksObj.image
+    const nationalPark = document.createElement('h3')
+    nationalPark.textContent = parksObj.nationalPark
+    const location = document.createElement('h4')
+    location.textContent = parksObj.location
+    const sunrise = document.createElement('p')
+    sunrise.textContent = parksObj.sunrise
+    const sunset = document.createElement('p')
+    sunset.textContent = parksObj.sunset
+    const goldenHour = document.createElement('p')
+    goldenHour.textContent = parksObj["golden-hour"]
+    const dayLength = document.createElement('p')
+    dayLength.textContent = parksObj["day-length"]
+    const like = document.createElement('button')
+    like.textContent = "Like"
+    sunriseList.append(li)
+    li.append(image, nationalPark, location, sunrise, sunset, goldenHour, dayLength, like)
 }
+
+function iterateParks(array) {
+    array.forEach(renderNationalParks)
+}
+
+// INITIALIZERS
+getNationalParks(URL).then(parksArray => {iterateParks(parksArray)})
+// getAllSunrises(37.865101, -119.538330, "PST").then(console.log)
+
