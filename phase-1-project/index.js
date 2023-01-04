8c7301bc02f4e4cd5100ffe5a454a42b1b8c0e3e
Sam Chappel <m.samchappel@gmail.com>
2023-01-03 16:11:36 -0800

    new draft to start from

--- phase-1-project/index.js
+++ phase-1-project/index.js
@@ -1,15 +1,17 @@
 // GLOBAL
 const baseUrl = "https://api.sunrisesunset.io/json?"
+const endUrl = "&date=today"
 const sunriseList = document.querySelector('#sunrise-list')
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+
 
 // FETCH FUNCTIONS
-function getAllSunrises(lat, lng) {
-    return fetch(baseUrl + `lat=${lat}&lng=${lng}`)
+function getAllSunrises(lat, lng, tmz) {
+    return fetch(baseUrl + `lat=${lat}&lng=${lng}&timezone=${tmz}` + endUrl)
         .then(response => response.json())
         .then(console.log)
 }
+
+https://api.sunrisesunset.io/json?lat=38.907192&lng=-77.036873&timezone=UTC&date=today
 
 // RENDER FUNCTIONS
 function addLikeBtn() {
